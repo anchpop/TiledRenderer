3a4e63c746cac784c4925e3952a21bc242060b5a
anchpop <andre@andreisawesome.com>
2016-11-09 21:27:21 -0500

    Initial commit

---
+++ Assets/Scripts/ObjectPlacer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using TiledSharp;
+
+
+
+public class ObjectPlacer : MonoBehaviour {
+
+    public bool createMapOnAwake = true;
+    public bool flipY = true;
+    //public List<Texture2D> sheets = new List<Texture2D>();
+    List<Sprite> spriteList = new List<Sprite> { null };
+    Dictionary<TmxLayerTile, GameObject> tileGameobjectMap = new Dictionary<TmxLayerTile, GameObject>();
+    TmxMap map;
+
+    void Start () {
+        if (createMapOnAwake) RenderMap("Assets/Resources/Tilemaps/test2.tmx");
+    }
+
+    public void RenderMap(string path)
+    {
+        map = new TmxMap(path);
+
+
+        foreach (var tileSet in map.Tilesets)
+        {
+            spriteList.AddRange(Resources.LoadAll<Sprite>("Tilesheets/" + tileSet.Name));
+        }
+
+        foreach (var layer in map.Layers)
+        {
+            foreach (var tile in layer.Tiles)
+            {
+                var destx = tile.X * (map.TileWidth/spriteList[tile.Gid].pixelsPerUnit);
+                var desty = (flipY ? -1 : 1) * tile.Y * (map.TileWidth / spriteList[tile.Gid].pixelsPerUnit);
+                addTileAsObject(tile, destx, desty);
+            }
+        }
+    }
+
+    void addTileAsObject(TmxLayerTile tile, float x, float y)
+    {
+        if (tile.Gid > 0)
+        {
+            var go = new GameObject("Tile");
+            go.transform.SetParent(transform);
+            go.transform.position = new Vector3(x, y, 0);
+
+            var sprend = go.AddComponent<SpriteRenderer>();
+            sprend.sprite = spriteList[tile.Gid];
+            sprend.flipX = tile.HorizontalFlip;
+            sprend.flipX = tile.VerticalFlip;
+
+            tileGameobjectMap[tile] = go;
+        }
+    }
+
+    TmxLayerTile getTmxTile(string layer, int gridLocationX, int gridLocationY)
+    {
+        return map.Layers[layer].Tiles.ToList().Find(t => t.X == gridLocationX && t.Y == gridLocationY);
+    }
+
+    GameObject getTile(string layer, int gridLocationX, int gridLocationY)
+    {
+        return tileGameobjectMap[map.Layers[layer].Tiles.ToList().Find(t => t.X == gridLocationX && t.Y == gridLocationY)];
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/readme.md
@@ -0,0 +1,17 @@
+How to use:
+
+1. Make a map in Tiled. (TODO insert link here). When using collisions, create them with the new Tiled Collision Editor, which supports complex polygons with holes.
+
+2. Ensure your tilesheets are placed somewhere within the Assets/Resources folder so you know they will be included in the final build
+
+3. Mark your tilesheets' Texture Type as Sprite in the import settings. Make sure the sprite mode is set to Multiple. Then open the Sprite Editor and slice up your sheets. If you have spritesheets of different sizes (which isn't recommended), you may need to adjust the pivot point. Set the Filter Mode to Point (No Filter) as well.
+
+4. Add the tilemap script to the Gameobject whose parent you want the tile objects to be!
+
+
+
+checklist
+empty tile support
+flipx, ✓ flipy, ✓ flipdiagonal support
+collision support
+Editor integration
